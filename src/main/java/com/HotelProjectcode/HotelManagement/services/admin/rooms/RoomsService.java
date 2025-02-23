afd6960f836ad2bcc1f728b9dd39f02f310e2147
Sriram1118 <115363944+Sriram1118@users.noreply.github.com>
2025-02-23 12:10:03 +0530

    added update / delete rooms api

--- src/main/java/com/HotelProjectcode/HotelManagement/services/admin/rooms/RoomsService.java
+++ src/main/java/com/HotelProjectcode/HotelManagement/services/admin/rooms/RoomsService.java
@@ -1,9 +1,18 @@
 package com.HotelProjectcode.HotelManagement.services.admin.rooms;
 
 import com.HotelProjectcode.HotelManagement.dto.RoomDto;
+import com.HotelProjectcode.HotelManagement.dto.RoomsResponseDto;
 
 public interface RoomsService {
 
     boolean postRoom(RoomDto roomDto);
+
+    RoomsResponseDto getAllRooms(int pageNumber);
+
+    RoomDto getRoomById(Long id);
+
+    boolean updateRoom(Long id, RoomDto roomDto);
+
+    void deleteRoom(Long id);
 
 }
--- src/main/java/com/HotelProjectcode/HotelManagement/services/admin/rooms/RoomsServiceImpl.java
+++ src/main/java/com/HotelProjectcode/HotelManagement/services/admin/rooms/RoomsServiceImpl.java
@@ -1,10 +1,18 @@
 package com.HotelProjectcode.HotelManagement.services.admin.rooms;
 
 import com.HotelProjectcode.HotelManagement.dto.RoomDto;
+import com.HotelProjectcode.HotelManagement.dto.RoomsResponseDto;
 import com.HotelProjectcode.HotelManagement.entity.Room;
 import com.HotelProjectcode.HotelManagement.repository.RoomRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -27,4 +35,50 @@ public class RoomsServiceImpl implements RoomsService {
             return false;
         }
     }
+
+    public RoomsResponseDto getAllRooms(int pageNumber){
+        Pageable pageable = PageRequest.of(pageNumber,6);
+        Page<Room> roomPage = roomRepository.findAll(pageable);
+
+        RoomsResponseDto roomsResponseDto = new RoomsResponseDto();
+        roomsResponseDto.setPageNumber(roomPage.getPageable().getPageNumber());
+        roomsResponseDto.setTotalPages(roomPage.getTotalPages());
+        roomsResponseDto.setRoomDtoList(roomPage.stream().map(Room::getRoomDto).collect(Collectors.toList()));
+
+        return roomsResponseDto;
+    }
+
+    public RoomDto getRoomById(Long id){
+        Optional<Room> optionalRoom = roomRepository.findById(id);
+        if(optionalRoom.isPresent()){
+            return optionalRoom.get().getRoomDto();
+        }else{
+            throw new EntityNotFoundException("Room not Present.");
+        }
+    }
+
+    public boolean updateRoom(Long id, RoomDto roomDto){
+        Optional<Room> optionalRoom = roomRepository.findById(id);
+        if(optionalRoom.isPresent()){
+            Room existingRoom = optionalRoom.get();
+            existingRoom.setName(roomDto.getName());
+            existingRoom.setPrice(roomDto.getPrice());
+            existingRoom.setType(roomDto.getType());
+
+            roomRepository.save(existingRoom);
+
+            return true;
+        }return false;
+    }
+
+    public void deleteRoom(Long id){
+        Optional<Room> optionalRoom = roomRepository.findById(id);
+        if(optionalRoom.isPresent()){
+            roomRepository.deleteById(id);
+        }else{
+            throw new EntityNotFoundException("Room Not Found");
+        }
+    }
+
+
 }
